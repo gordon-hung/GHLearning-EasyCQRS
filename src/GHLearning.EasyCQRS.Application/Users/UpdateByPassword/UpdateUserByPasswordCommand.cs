0090b53e7b0ff3784a4cf46af39d53cda703ca23
gordon.hung <a932025@gmail.com>
2025-05-25 08:39:44 +0800

    新增使用者密碼更新命令及處理器
    新增 `UpdateUserByPasswordCommand` 類別以定義用戶名和密碼屬性。實作 `UpdateUserByPasswordCommandHandler` 類別以處理密碼更新邏輯，包括從資料庫獲取用戶、哈希密碼及更新用戶資料的功能。

---
+++ src/GHLearning.EasyCQRS.Application/Users/UpdateByPassword/UpdateUserByPasswordCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GHLearning.EasyCQRS.Application.Abstractions.Messaging;
+using GHLearning.EasyCQRS.Application.Users.Create;
+
+namespace GHLearning.EasyCQRS.Application.Users.UpdateByPassword;
+public record UpdateUserByPasswordCommand(
+	string Username,
+	string Password) : ICommand;
---
+++ src/GHLearning.EasyCQRS.Application/Users/UpdateByPassword/UpdateUserByPasswordCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GHLearning.EasyCQRS.Application.Abstractions.Messaging;
+using GHLearning.EasyCQRS.Core.Users;
+using GHLearning.EasyCQRS.Core.Users.Models;
+using GHLearning.EasyCQRS.SharedKernel;
+using Microsoft.Extensions.Logging;
+
+namespace GHLearning.EasyCQRS.Application.Users.UpdateByPassword;
+internal class UpdateUserByPasswordCommandHandler(
+	TimeProvider timeProvider,
+	IPasswordHasher passwordHasher,
+	IUserRepository userRepository) : ICommandHandler<UpdateUserByPasswordCommand>
+{
+	public async Task Handle(UpdateUserByPasswordCommand command, CancellationToken cancellationToken)
+	{
+		var user = await userRepository.GetByUsernameAsync(command.Username, cancellationToken).ConfigureAwait(false)
+			?? throw new ArgumentException($"User with username '{command.Username}' does not exist.", nameof(command.Username));
+
+		var passwordHash = passwordHasher.HashPassword(command.Password);
+
+		var operationAt = timeProvider.GetUtcNow();
+
+		await userRepository.UpdateByPasswordAsync(
+			parameter: new UpdatedByPassword(
+				Code: user.Code,
+				Password: passwordHash,
+				OperationAt: operationAt),
+			cancellationToken: cancellationToken)
+			.ConfigureAwait(false);
+	}
+}
